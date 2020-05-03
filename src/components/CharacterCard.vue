f56a9a6406071c3dc93357db1b70d6b08e4193bc
Dre Shannon <leandre.shannon@centurylink.com>
2020-05-03 09:40:08 -0500

    Create CharacterCard.vue

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card>
+    <div class="card">
+      <div>
+        <v-card-title
+          class="headline"
+          v-text="character.name"
+        />
+        <v-card-subtitle>
+          <v-icon>mdi-security</v-icon> - {{ character.defense }}
+          <v-icon>mdi-heart</v-icon> - {{ character.health }}
+          <p>{{ character.abilityName }}</p>
+          <p>{{ character.abilityDescription }}</p>
+        </v-card-subtitle>
+      </div>
+      <v-avatar
+        class="ma-3"
+        size="125"
+        tile
+      >
+        <!-- <v-img :src="character.picture" /> -->
+        <v-img src="@/assets/Kisha.png" />
+      </v-avatar>
+    </div>
+    <v-card-actions>
+      <v-btn>Select</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+import {Component, Vue, Prop} from 'vue-property-decorator';
+
+interface Character {
+  name: string;
+  health: number;
+  defense: number;
+  abilityName: string;
+  abilityDescription: string;
+  picture: string;
+}
+
+@Component
+export default class CharacterCard extends Vue {
+  @Prop() character!: Character;
+}
+</script>
+
+<style lang="scss" scoped>
+div.v-card {
+  border: 1em solid #EAE7E0;
+  background: rgb(56,88,129);
+  background: linear-gradient(0deg, rgba(56,88,129,1) 0%, rgba(108,145,172,1) 50%, rgba(56,88,129,1) 100%);
+}
+</style>

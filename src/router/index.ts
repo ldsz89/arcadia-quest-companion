e471068d04878f231824f618af6565b9ad03dc80
Dre Shannon <leandre.shannon@centurylink.com>
2020-05-03 09:40:00 -0500

    Adds character selection to navigation

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter, {RouteConfig} from 'vue-router';
 import Home from '../views/Home.vue';
+import SelectCharacter from '@/views/SelectCharacter.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/select-character',
+    name: 'Select Character',
+    component: SelectCharacter,
   },
   {
     path: '/about',
---
+++ src/views/SelectCharacter.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <v-row dense>
+      <v-col
+        v-for="(character, i) in characters"
+        :key="i"
+        cols="12"
+      >
+        <character-card :character="character" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+import CharacterCard from '@/components/CharacterCard.vue';
+import characters from '@/data/characters.json';
+
+@Component({
+  components: {
+    CharacterCard,
+  },
+})
+export default class SelectCharcter extends Vue {
+  characters = characters;
+}
+</script>
+
+<style lang="scss">
+.v-card div.card {
+  display: flex;
+  flex-flow: nowrap;
+  justify-content: space-between;
+
+  .v-card__subtitle {
+    text-align: left;
+  }
+}
+</style>

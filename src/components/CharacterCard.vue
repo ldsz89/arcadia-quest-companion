57425a79ad0985a738ee40513a3339caa6533bcc
Dre Shannon <leandre.shannon@centurylink.com>
2020-05-05 23:19:41 -0500

    Makes character card heights uniform

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -44,6 +44,7 @@ export default class CharacterCard extends Vue {
 
 <style lang="scss" scoped>
 div.v-card {
+  height: 15em;
   border: 1em solid #EAE7E0;
   background: rgb(56,88,129);
   background: linear-gradient(0deg, rgba(56,88,129,1) 0%, rgba(108,145,172,1) 50%, rgba(56,88,129,1) 100%);

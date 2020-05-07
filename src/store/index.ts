672e30523f0a0bfb54e81cd7f6d03020d8fd214e
Dre Shannon <leandre.shannon@centurylink.com>
2020-05-06 21:31:31 -0500

    Sets guild store object with name mutations

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,49 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import {Guild, GuildMember, Character, Equipment} from '../types';
+
+const guild: Guild = {
+  name: '',
+  guildMembers: [],
+};
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    guild,
+  },
+  mutations: {
+    setGuildName(state, name: string) {
+      state.guild.name = name;
+    },
+    setGuildMembers(state, guildMembers: GuildMember[]) {
+      state.guild.guildMembers = guildMembers;
+    },
+    addGuildMembers(state, guildMember: GuildMember) {
+      if (state.guild.guildMembers.indexOf(guildMember) === -1) {
+        state.guild.guildMembers.push(guildMember);
+      }
+    },
+    removeGuildMember(state, guildMember: GuildMember) {
+      if (state.guild.guildMembers.indexOf(guildMember) !== -1) {
+        state.guild.guildMembers.splice(state.guild.guildMembers.indexOf(guildMember), 1);
+      }
+    },
+    clearGuildMembers(state) {
+      state.guild.guildMembers = [];
+    },
+    // setGuildMemberCharacter(state, guildMember: GuildMember, character: Character) {
+    //   const guildMemberIndex = state.guild.guildMembers.indexOf(guildMember);
+    //   if (guildMemberIndex !== -1) {
+    //     state.guild.guildMembers[guildMemberIndex].character = character;
+    //   }
+    // },
+    // setGuildMemberEquipment(state, guildMember: GuildMember, equipment: Equipment[]) {
+    //   const guildMemberIndex = state.guild.guildMembers.indexOf(guildMember);
+    //   state.guildMembers[guildMemberIndex].equipment = equipment;
+    // },
+  },
   actions: {},
   modules: {},
 });

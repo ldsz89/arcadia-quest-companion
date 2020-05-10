df0d4c148f9d62658449ed6f7ae3a6085c9c9c6a
Dre Shannon <leandre.shannon@centurylink.com>
2020-05-10 18:31:45 -0500

    Creates Crit Icon component

---
+++ src/components/CritIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="crit">
+    <v-icon>mdi-alert-octagram-outline</v-icon>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+
+@Component
+export default class CritIcon extends Vue {
+}
+</script>
+
+<style lang="scss">
+.v-icon {
+  font-size: 3em;
+  color: white;
+  background-color: black;
+  border-radius: 17px;
+  padding: 2px;
+}
+</style>

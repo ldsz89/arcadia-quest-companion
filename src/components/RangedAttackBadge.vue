47f313e2f32be7a31fdb26a3a2605be999057dd1
Dre Shannon <leandre.d.shannon@gmail.com>
2020-05-10 22:24:54 -0500

    Creates sword and bow images and component badges

---
+++ src/components/RangedAttackBadge.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-badge
+    :content="attack"
+    color="rgba(0, 0, 0, 0.5)"
+    overlap
+    bottom
+    left
+  >
+    <v-img
+      src="../assets/Bow.png"
+      width="2.5em"
+    />
+  </v-badge>
+</template>
+
+<script lang="ts">
+import {Component, Vue, Prop} from 'vue-property-decorator';
+
+@Component
+export default class RangedAttackBadge extends Vue {
+  @Prop() attack!: number;
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
